afbfa04aa6b31cbc0e5fbe6a4f693fa38c5a0a3c
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 09:50:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,46 +1,29 @@
-class CashRegister
-  attr_accessor :discount, :total
-  
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @cart = []
+lass CashRegister
+  attr_accessor :total, :discount, :items, :last
+  def initialize(discount=0)
+    self.total = 0
+    self.discount = discount
   end
 
-  def add_item(item, price, quantity = 1)
-    item_info = {}
-    item_info[:name] = item
-    item_info[:price] = price
-    item_info[:quantity] = quantity
-
-    @cart << item_info
-
-    @total += price * quantity
-
+  def add_item(title, price, quantity=1)
+    self.items ||= []
+    quantity.times { self.items << title }
+    self.last = price * quantity
+    self.total += self.last
   end
 
   def apply_discount
-    if @discount == 0
-      return "There is no discount to apply."
+    if self.discount == 0
+      "There is no discount to apply."
+    else
+      percentage = self.discount / 100.0
+      self.total -= self.total * percentage
+      "After the discount, the total comes to $#{self.total.floor}."
     end
-    @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
-  end 
+  end
 
-  def items
-    item_names = []
-    @cart.each do | item_info |
-      #test expects product name * quantity...
-      for qty in 1..item_info[:quantity] 
-        item_names << item_info[:name]
-      end 
-    end 
-    item_names
-  end 
-  
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total -= self.last
   end
 
 end
